78a5cdb2ee594164076615bd7cb0d3e9069ec2cf
davidjpmoore <davidjpmoore@email.arizona.edu>
2015-03-05 18:54:15 -0700

    CLEANED input script to read data from iplant repository
    Dave, 03/05/2015

--- input script.R
+++ input script.R
@@ -1,130 +1,34 @@
-dat = read.csv("enzymeFlux.csv",header=TRUE)
-BGCdat = read.csv("rHighPark_DOC_TDN_Corrected.csv",header=TRUE)
-#remove GIGANTIC LAP_15C
-dat$LAP_15C[which(dat$LAP_15C>20000)]<-NA
+# This script will assume that you have a file structure that includes a folder called "data" in your rProject folder
+# If you don't ... create one ... then download all the datafiles from iPlant
 
-Sdate = as.Date(strDates,"%m/%d/%y")
-dat$Sdate = Sdate;
-
-
-BGCSdate = as.Date(ISOdate(BGCdat$Year,BGCdat$Month, BGCdat$Day)) 
-BGCdat$Sdate = BGCSdate
-
-merge(BGCdat, dat, by = intersect(names(SAMPLEIDPLOT), names(y)),
-      
-      
-      merge(BGCdat, dat, by = intersect(names(Sdate), names(SAMPLEIDPLOT)), all =TRUE)
-            by.x = by, by.y = by, all = FALSE, all.x = all, all.y = all,
-            sort = TRUE, suffixes = c(".x",".y"),
-            incomparables = NULL, ...)
+#all files are dot.csv files - you can open them any number of ways - I've opted to use read.csv
+#data files that are dot.xlsx might be interesting too but they are not pulled into R - they should contain identical data to the dot.csv files
+#information about the headers and contents of each file is contained within the associated README.txt file (with the shared filename)
 
 
-apple = merge(BGCdat,dat,by="Sdate","SAMPLEIDPLOT",all=T)
-#merging failed ... SAMPLEIDPLOT from dat is not found ... even though it appears to be there. 
-#it's possible that these values are not the same across both datasets
+#read in Biogeochemistry data file into dataframe "BGCdat" - 72 observations
+BGCdat = read.csv("data/HighPark_BGCpools_MASTER_r.csv",header=TRUE)
+#TO DO 03/05/3015: CONTAINS ONLY LSP, SPR, UMC - MUST ADD NWT & FEF FROM NICOLE
 
 
-df3<-merge(df1,df2,by="a",all=T
+#read in MASTER disturbance information into dataframe "DISdef" (no actual data here just plot definitions) - 98 observations
+DISdef = read.csv("data/MASTERPLOT_DISTURBANCE_INFO.csv",header=TRUE)
 
-intersect(names(Sdate), names(SAMPLEIDPLOT)), all =TRUE)
-by.x = by, by.y = by, all = FALSE, all.x = all, all.y = all,
-sort = TRUE, suffixes = c(".x",".y"),
-incomparables = NULL, ...)
+#read in Enzyme activity data file into dataframe "ENZdat" - 178 observations
+ENZdat = read.csv("data/MicrobialExoEnzymeActivities_COMASTER_README.csv",header=TRUE)
 
-
-
-plot(dat$BG_15C[dat$LAP_15C<1000],dat$LAP_15C[dat$LAP_15C<1000])
-
+#read in Licor Efflux data file into dataframe "FLUXdat" - 1677 observations
+#note Noelle - you might model your River Road data structure on this file
+FLUXdat = read.csv("data/RAPID2013_MasterFluxes.csv",header=TRUE)
 
 
-plot(dat$BG_15C)
+#read in Pyrolysis summary data file into dataframe "PYRdat" - 114 observations
+PYRdat = read.csv("data/PyrolysisAllSitesRelativeProportions_MASTER.csv",header=TRUE)
 
-plot (dat$BG_15C[dat$site=='UMC'],dat$BG_15C[dat$site=='UMC'])
-plot (dat$BG_15C[dat$site=='UMC'],dat$DOC[dat$site=='UMC'])
-plot (dat$TDN[dat$site=='UMC'],dat$BG_15C[dat$site=='UMC'])
-plot (dat$TDN[dat$site=='UMC'],dat$LAP_15C[dat$site=='UMC'])
-plot (dat$MicrobialBiomassC[dat$site=='UMC'],dat$BG_15C[dat$site=='UMC'])
-plot (dat$Tsoil_C[dat$site=='UMC'],dat$EFFLUX[dat$site=='UMC'])
-plot (dat$Tsoil_C,dat$EFFLUX)
+#to do:
+#read in the series of topographic data files from Rebecca's analysis
 
-tapply(dat$LAP_15C,dat$site,mean, na.rm=TRUE)
-
-tapply(BGCdat$ug.PO43..P.g.1.dry,BGCdat$Site,mean, na.rm=TRUE)
-
-plot(BGCdat$Site,BGCdat$ug.PO43..P.g.1.dry)
-plot(BGCdat$Site,BGCdat$NO3.)
-plot(BGCdat$Site,BGCdat$NH4.)
-plot(BGCdat$Site,BGCdat$DIN)
-plot(BGCdat$Site,BGCdat$Biomass_C_final)
-
-plot(dat$site,dat$EFFLUX)
-
-a <- ggplot(dat, aes(x=Tsoil_C, y=EFFLUX))
-a + geom_point()
-a + geom_point(aes(colour = BG_15C))
-a + geom_point(colour = "red", size = 6)
-a + geom_point(colour="grey50", size = 4) + geom_point(aes(colour = pH)) + scale_colour_gradient(low = "yellow", high="red")
-a + geom_point(aes(shape = factor(site)))
-a + geom_point(aes(size = PCntcover, colour=pH))
-a + geom_point(aes(size = pH)) + scale_area()
-a + geom_point(aes(fill=pH, size=pH), colour="black", shape=21) + scale_size(range = c(1, 20))
-
-p  <- ggplot(dat, aes(x=pH, y=EFFLUX))
-p + geom_point(aes(fill=Tsoil_C, size=Tsoil_C), colour="black", shape=21) + scale_size(range = c(1, 20))
-
-
-p + geom_point(aes(fill=PCntcover, size=PCntcover), colour="black", shape=21) + scale_size(range = c(1, 20))
-
-p + geom_point(aes(fill=BG_15C, size=BG_15C), colour="black", shape=21) + scale_size(range = c(1, 20))
-p + geom_point(aes(fill=CB_15C, size=CB_15C), colour="black", shape=21) + scale_size(range = c(1, 20))
-p + geom_point(aes(fill=AG_15C, size=AG_15C), colour="black", shape=21) + scale_size(range = c(1, 20))
-p + geom_point(aes(fill=XYL_15C, size=XYL_15C), colour="black", shape=21) + scale_size(range = c(1, 20))
-
-
-p + geom_point(aes(fill=LAP_35C, size=LAP_35C), colour="black", shape=21) + scale_size(range = c(1, 20))
-p + geom_point(aes(fill=NAG_15C, size=NAG_15C), colour="black", shape=21) + scale_size(range = c(1, 20))
-
-
-b <- ggplot(dat, aes(x=BG_15C, y=EFFLUX))
-b + geom_point(colour = "red", size = 6)
-b + geom_point( size = 5) + geom_point(aes(colour = pH)) + scale_colour_gradient(low = "white", high="black")
-b + geom_point(aes(shape = factor(site))) + geom_point(aes(size =2+pH))
-               
-c <- ggplot(dat, aes(x=pH, y=BG_15C))
-c + geom_point(colour = "red", size = 4)        
-c + geom_point(colour="grey50", size = 6) + geom_point(aes(colour = EFFLUX)) + scale_colour_gradient(low = "yellow", high="red")
-
-c + geom_point(aes(fill=EFFLUX, size=EFFLUX), colour="black", shape=21)
-
-+ scale_colour_gradient(low = "yellow", high="red")
-
-
-
-
-
-hist(dat$BG_15C, breaks = "Sturges",
-     freq = NULL, probability = !freq,
-     include.lowest = TRUE, right = TRUE,
-     density = NULL, angle = 45, col = NULL, border = NULL,
-     main = paste("Histogram of" , xname),
-     xlim = range(breaks), ylim = NULL,
-     xlab = xname, ylab,
-     axes = TRUE, plot = TRUE, labels = FALSE,
-     nclass = NULL, warn.unused = TRUE)
-
-
-
-reg1 = lm(EFLUX_bp ~ OL_bp)   #model syntax: y ~ x
-reg1    		     # default return from lm
-summary(reg1)			     # detailed summary of results
-anova(reg1)			     # ANOVA table of results
-plot(reg1)			     # diagnostic plots (4 panels)
-plot(residuals(reg1))		# residuals by row
-coef(reg1)			# parameter coefficients
-vcov(reg1)			# parameter covariance matrix
-plot(EFLUX_bp, OL_bp)
-abline(reg1)			# adding regression line to the scatter plot
-
-
-
-
+#next steps:
+#explore and plot variables within each data frame
+#calculate summary statistics for key variables
+#merge key variables to allow regression analysis
